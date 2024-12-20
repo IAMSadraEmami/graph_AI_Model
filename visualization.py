23b4690bc76b3c82b20cdc0f1e6065970427f81c
Julian-JP <julian.laux@tum.de>
2024-12-20 13:30:47 +0100

    Simple adjacency-matrix visualization

---
+++ visualization.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import networkx as nx
+
+def plot_graph(adjmatrix):
+    nx_graph = nx.Graph()
+
+    for i in range(len(adjmatrix)):
+        nx_graph.add_node(i)
+
+    for i, row in enumerate(adjmatrix):
+        for j, entry in enumerate(row):
+            if entry != 0:
+                nx_graph.add_edge(i, j, weight=entry)
+
+    pos = nx.spring_layout(nx_graph)
+    labels = nx.get_edge_attributes(nx_graph, 'weight')
+    nx.draw(nx_graph, pos, with_labels=True, node_size=700, node_color='lightblue', font_size=10)
+    nx.draw_networkx_edge_labels(nx_graph, pos, edge_labels=labels)
+    plt.show()
+
+
+if __name__ == "__main__":
+    adjmatrix = [[0, 1, 0], [1, 0, 1], [0, 1, 0]]
+    plot_graph(adjmatrix)
